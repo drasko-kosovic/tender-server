257e06e856b868000a00fc2f406e001b93630900
drasko1959 <drasko.kosovic@gmail.com>
2021-01-08 21:46:06 +0100

    init

---
+++ src/main/java/com/tender/models/Pregovaracki.java
@@ -0,0 +1,37 @@
+package com.tender.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "pregovaraci")
+public class Pregovaracki {
+
+    String atc;
+    String naziv_proizvoda;
+    Integer trazena_kolicina;
+    Integer ponudjana_kolicina;
+    Double procijenjena_jedinicna_cijena;
+    Double ponudjena_jedinicna_cijena;
+    Double procijenjena_ukupna_cijena;
+    Double ponudjena_ukupna_cijena;
+    String ponudjac;
+    Integer broj_jednostavne;
+    String broj_ugovora_odluka;
+    Date datum_ugovora_odluke;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private Boolean selected;
+
+    public Pregovaracki() {
+    }
+
+
+    
+
+
+}
